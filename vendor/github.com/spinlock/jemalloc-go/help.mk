3804f7a560f6330a0bd1e28f5189ae29786972ef
zhanghao <zhanghao997@qq.com>
2024-02-02 18:41:19 +0800

    go mod

--- vendor/github.com/spinlock/jemalloc-go/help.mk
+++ vendor/github.com/spinlock/jemalloc-go/help.mk
@@ -1,15 +1,16 @@
 .DEFAULT_GOAL = relink
 
-PWD := $(shell pwd)
-SRC := jemalloc-4.4.0
+SOURCE := jemalloc-4.4.0
 
--include $(SRC)/Makefile
+-include $(SOURCE)/Makefile
 
-relink: unlink
+relink: rmlink
 	@for i in $(C_SRCS); do \
-		rm -f            je_$$(basename $$i); \
-		ln -s $(SRC)/$$i je_$$(basename $$i); \
+		rm -f               je_$$(basename $$i); \
+		ln -s $(SOURCE)/$$i je_$$(basename $$i); \
 	done
+	@ln -s $(SOURCE)/VERSION VERSION
+	@ln -s $(SOURCE)/include/jemalloc jemalloc
 
-unlink:
-	@rm -f je_*.c
+rmlink:
+	@rm -f je_*.c jemalloc VERSION
